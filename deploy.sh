d278cd3813b888459ece5eeb65b348d3f995f018
david <david@persistentdesigns.com>
2018-11-01 13:03:05 -0400

    corrected SHA in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t davidsells/multi-client -t davidsells/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t davidsells/multi-server -t davidsells/multi-server  -f ./server/Dockerfile ./server
-docker build -t davidsells/multi-worker -t davidsells/multi-worker  -f ./worker/Dockerfile ./worker
+docker build -t davidsells/multi-server -t davidsells/multi-server:$SHA  -f ./server/Dockerfile ./server
+docker build -t davidsells/multi-worker -t davidsells/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
 
 docker push davidsells/multi-client:latest
 docker push davidsells/multi-server:latest
