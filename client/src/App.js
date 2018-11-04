87bc6bd439f98f615a0c65b67b847a248cb2ae04
david <david@persistentdesigns.com>
2018-11-04 15:45:24 -0500

    changed title III

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-	    <h1>Fib Calculator Kubernetes Version</h1>
+	    <h1>Fib Calculator Kubernetes Version III</h1>
           </p>
           <a
             className="App-link"
